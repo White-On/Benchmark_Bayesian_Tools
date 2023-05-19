f3cd2ce98598ec47a59e947f75eb983ac4505975
White-On <74248238+White-On@users.noreply.github.com>
2023-05-19 15:49:36 +0200

    updating task

---
+++ themes/Learning/Learning_exaustive_search_BIC/evaluation.py
@@ -0,0 +1,17 @@
+import pyAgrum as gum
+import pyAgrum.lib.bn_vs_bn as gcm
+import os
+
+def evaluateFscore(filenameBif, libraryName, arg):
+    curentdir = os.path.dirname(os.path.realpath(__file__))
+    bn = gum.loadBN(f"{curentdir}/data/{filenameBif}")
+
+    # we check if the file exists
+    filenameBnLearned = f"{curentdir}/data/{libraryName}_learned_{arg}_{filenameBif}"
+    if not os.path.isfile(filenameBnLearned):
+        return None
+    bnlearn = gum.loadBN(filenameBnLearned)
+
+    cmp=gcm.GraphicalBNComparator(bn,bnlearn)
+
+    return cmp.scores()["fscore"]
