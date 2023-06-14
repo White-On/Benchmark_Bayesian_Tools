130a732ca8a514d15a975a7d0d862c677e057621
White-On <74248238+White-On@users.noreply.github.com>
2023-06-14 16:44:38 +0200

    update evalution

--- themes/Learning/Learning_best_method/evaluation.py
+++ themes/Learning/Learning_best_method/evaluation.py
@@ -15,3 +15,31 @@ def evaluateFscore(filenameBif, libraryName, arg):
     cmp=gcm.GraphicalBNComparator(bn,bnlearn)
 
     return cmp.scores()["fscore"]
+
+def evaluateRecall(filenameBif, libraryName, arg):
+    curentdir = os.path.dirname(os.path.realpath(__file__))
+    bn = gum.loadBN(f"{curentdir}/data/{filenameBif}")
+
+    # we check if the file exists
+    filenameBnLearned = f"{curentdir}/data/{libraryName}_learned_{arg}_{filenameBif}"
+    if not os.path.isfile(filenameBnLearned):
+        return None
+    bnlearn = gum.loadBN(filenameBnLearned)
+
+    cmp=gcm.GraphicalBNComparator(bn,bnlearn)
+
+    return cmp.scores()["recall"]
+
+def evaluatePrecision(filenameBif, libraryName, arg):
+    curentdir = os.path.dirname(os.path.realpath(__file__))
+    bn = gum.loadBN(f"{curentdir}/data/{filenameBif}")
+
+    # we check if the file exists
+    filenameBnLearned = f"{curentdir}/data/{libraryName}_learned_{arg}_{filenameBif}"
+    if not os.path.isfile(filenameBnLearned):
+        return None
+    bnlearn = gum.loadBN(filenameBnLearned)
+
+    cmp=gcm.GraphicalBNComparator(bn,bnlearn)
+
+    return cmp.scores()["precision"]
