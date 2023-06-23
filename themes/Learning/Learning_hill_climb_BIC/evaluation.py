9276e2e9fada925e0f5fbf3e648b17289b4986ed
White-On <74248238+White-On@users.noreply.github.com>
2023-06-23 15:11:47 +0200

    adding the evaluation methods

--- themes/Learning/Learning_hill_climb_BIC/evaluation.py
+++ themes/Learning/Learning_hill_climb_BIC/evaluation.py
@@ -16,3 +16,34 @@ def evaluateFscore(filenameBif, libraryName, arg):
     cmp = gcm.GraphicalBNComparator(bn, bnlearn)
 
     return cmp.scores()["fscore"]
+
+def evaluateRecall(filenameBif, libraryName, arg):
+    """Evaluate the Recall of a learned BN"""
+    curentdir = os.path.dirname(os.path.realpath(__file__))
+    bn = gum.loadBN(f"{curentdir}/data/{filenameBif}")
+
+    # we check if the file exists
+    filenameBnLearned = f"{curentdir}/data/{libraryName}_learned_{arg}_{filenameBif}"
+    if not os.path.isfile(filenameBnLearned):
+        return None
+    bnlearn = gum.loadBN(filenameBnLearned)
+
+    cmp = gcm.GraphicalBNComparator(bn, bnlearn)
+
+    return cmp.scores()["recall"]
+
+
+def evaluatePrecision(filenameBif, libraryName, arg):
+    """"""
+    curentdir = os.path.dirname(os.path.realpath(__file__))
+    bn = gum.loadBN(f"{curentdir}/data/{filenameBif}")
+
+    # we check if the file exists
+    filenameBnLearned = f"{curentdir}/data/{libraryName}_learned_{arg}_{filenameBif}"
+    if not os.path.isfile(filenameBnLearned):
+        return None
+    bnlearn = gum.loadBN(filenameBnLearned)
+
+    cmp = gcm.GraphicalBNComparator(bn, bnlearn)
+
+    return cmp.scores()["precision"]
