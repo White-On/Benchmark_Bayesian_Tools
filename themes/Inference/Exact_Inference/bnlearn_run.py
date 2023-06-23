b93dcf746af9fe35ca273504413a359e8831baf4
White-On <74248238+White-On@users.noreply.github.com>
2023-06-23 15:25:54 +0200

    quick chnage

--- themes/Inference/Exact_Inference/bnlearn_run.py
+++ themes/Inference/Exact_Inference/bnlearn_run.py
@@ -2,6 +2,7 @@ import os
 import bnlearn as bn
 import sys
 
+# doest not work because the memory is not freed
 currentdir = os.path.dirname(os.path.realpath(__file__))
 
 model = bn.import_DAG(f"{currentdir}/data/{sys.argv[1]}", verbose=0)
