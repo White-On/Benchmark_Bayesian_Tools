36d608c3cae6bfb743c6c7831fb57c06f920c2eb
White-On <74248238+White-On@users.noreply.github.com>
2023-07-19 15:07:20 +0200

    minor fix

--- themes/Learning/Learning_structure_large_sampling/bnlearn_before_run.py
+++ themes/Learning/Learning_structure_large_sampling/bnlearn_before_run.py
@@ -9,6 +9,6 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 # Load the BN to compare properly time of learning
 true_model = bn.import_DAG(f"{current_dir}/data/alarm.bif", verbose=False)
 
-# BIFWriter(true_model["model"]).write_bif(
-#     f"{current_dir}/data/outOfContext.bif"
-# )
+BIFWriter(true_model["model"]).write_bif(
+    f"{current_dir}/data/outOfContext.bif"
+)
