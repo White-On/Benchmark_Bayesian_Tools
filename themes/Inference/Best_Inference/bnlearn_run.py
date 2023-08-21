dd3263169cddbe2a75eb5962a439cc1400c95dc8
White-On <74248238+White-On@users.noreply.github.com>
2023-08-21 17:00:53 +0200

    fixing issue for bnlearn a test

--- themes/Inference/Best_Inference/bnlearn_run.py
+++ themes/Inference/Best_Inference/bnlearn_run.py
@@ -2,10 +2,10 @@ import os
 import bnlearn as bn
 import sys
 
-# doest not work because the memory is not freed
 currentdir = os.path.dirname(os.path.realpath(__file__))
 
 model = bn.import_DAG(f"{currentdir}/data/{sys.argv[1]}", verbose=0)
 
 for node in model["model"].nodes():
-    query = bn.inference.fit(bn, variables=[node], evidence={}, verbose=0)
+    query = bn.inference.fit(model, variables=[node], evidence={}, verbose=0)
+
