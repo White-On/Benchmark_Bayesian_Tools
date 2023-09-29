edd3d590eaa2b0ade65fb070829bb2eb2b2e545b
White-On <74248238+White-On@users.noreply.github.com>
2023-09-29 20:10:30 +0200

    deleted

--- themes/Inference/Variable_elimination/bnlearn/before.py
+++
@@ -1,7 +0,0 @@
-import bnlearn as bn
-import sys
-from pathlib import Path
-
-data_path = Path(__file__).parents[1] / "data" / f'{sys.argv[1]}'
-
-model = bn.import_DAG(data_path.__str__(), verbose=0)
